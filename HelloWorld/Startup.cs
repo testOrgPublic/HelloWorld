6558682fa50b795ee378df26425df55cea4bbee4
sinchandoshi <sinchand@SINCHAND-W8>
2017-03-02 15:25:58 +0530

    Add project files.

---
+++ HelloWorld/Startup.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(HelloWorld.Startup))]
+
+namespace HelloWorld
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
