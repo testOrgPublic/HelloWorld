bf80073d08df170ff001dc5a8efaaac24fc7c213
DOSHI <S.Doshi@iaea.org>
2023-12-20 16:03:17 +0100

    added secrets in code

--- HelloWorld/App_Start/BundleConfig.cs
+++ HelloWorld/App_Start/BundleConfig.cs
@@ -1,6 +1,9 @@
 ﻿using System.Web;
 using System.Web.Optimization;
 
+String tenantID = "12a345bc-67d1-ef89-abcd-efg12345abcde";
+String client_secret = "abcd~a1234bcd56789012abcdabcd1234abcd";
+
 namespace HelloWorld
 {
     public class BundleConfig
